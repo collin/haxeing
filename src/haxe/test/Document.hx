9d25fba0ab2bc2258057f4f50f53b0d1b30b5852
collin <collintmiller@gmail.com>
2009-03-20 12:04:58 -0500

    skeleton of a document model

--- src/haxe/test/Document.hx
+++ src/haxe/test/Document.hx
@@ -1,4 +1,53 @@
-class Document extends Xml {
+import Box;
+
+class Document {
+  dynamic var box;
+
   public function new() {
+    this.box = flash.Lib.current();
   }
 }
+
+class Node {
+  dynamic var box : Box;
+  dynamic var document   : Document;
+  dynamic var parentNode : Node;
+  dynamic var childNodes : Array<Node>;
+    
+  public function new(document) {
+    this.document = document;
+    this.box = Box.new();
+  }
+  
+  public function append(node) {
+    if(!node.parentNode == null) node.parentNode.removeChild(node);
+    node.parentNode = this;
+    this.childNodes.push(node);
+    this.reflow();
+  }
+  
+  public function appendTo(node) {
+    node.append(this);
+  }
+  
+  public function removeChild(node) {
+    node.parentNode = null;
+    this.childNodes = this.childrenWithout(node);
+    this.reflow();
+  }
+  
+  public function siblings() {
+    return this.parentNode.childrenWithout(self);
+  }
+  
+  private function childrenWithout(node) {
+    return this.childNodes.filter(function(child) {
+      return child != node;
+    });
+  }
+
+  private function reflow() {
+    // TODO
+  }
+}
+
